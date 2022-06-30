17bd57593cf99d2ec60dfb7928b2eebb73a44e41
satyamawasthi-cedcommerce <satyamawasthi@cedcoss.com>
2022-06-30 16:54:37 +0530

    template-3

---
+++ src/components/Card.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+export default class Card extends Component {
+  render() {
+    return (
+     
+          <div className="card">
+         
+          <div className="container">
+            <h2 className='title'><b>Console</b></h2> <hr/>
+            <p className='title'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                when an unknown printer took a galley of type and scrambled it to make a type 
+                specimen book. It has survived not only five centuries, but also the leap into 
+                electronic typesetting, remaining essentially unchanged. </p> <br/>
+                <hr/>
+                <h2 className='btn'> Go Anywhere </h2>
+     </div>
+     </div>
+    
+    )
+  }
+}
